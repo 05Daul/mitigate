e12d2dbcbf6cd84a8033d8593e614c2f391c4c82
Kim Daul <elre519@naver.com>
2024-12-18 11:22:44 +0900

    gate

---
+++ src/main/java/com/example/mimigate/MimigateApplication.java
@@ -0,0 +1,13 @@
+package com.example.mimigate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MimigateApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MimigateApplication.class, args);
+    }
+
+}
